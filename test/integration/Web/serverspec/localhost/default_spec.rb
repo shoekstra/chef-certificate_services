19bf5dd0ebe4f3e38c3099c2926a4ba37db7b615
Stephen Hoekstra <shoekstra@schubergphilis.com>
2016-03-13 10:31:44 +0100

    Fix iis_website tests

--- test/integration/Web/serverspec/localhost/default_spec.rb
+++ test/integration/Web/serverspec/localhost/default_spec.rb
@@ -4,26 +4,27 @@ require 'infrataster/rspec'
 Infrataster::Server.define(:localhost, '127.0.0.1')
 
 describe 'A Web Server' do
-  describe 'acting as a CDP' do
+  describe 'acting as a CRL Distribution Point' do
     describe 'has IIS installed' do
-      [
-        'Web-Mgmt-Tools',
-        'Web-WebServer'
-      ].each do |feature|
-        describe windows_feature(feature) do
+      %w(Web-Mgmt-Tools Web-WebServer).each do |feature_name|
+        describe windows_feature(feature_name) do
           it { should be_installed.by('powershell') }
         end
       end
 
-      describe service('WMSvc') do
-        it { should be_installed }
-        it { should be_running }
+      %w(W3SVC WMSvc).each do |service_name|
+        describe service(service_name) do
+          it { should be_installed }
+          it { should be_running }
+        end
       end
 
-      describe iis_app_pool('DefaultAppPool') do
-        it { should exist }
+      describe windows_registry_key('HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\WebManagement\Server') do
+        it { should have_property_value('EnableRemoteManagement', :type_string, '1') }
       end
+    end
 
+    describe 'has a /cdp virtual directory in the Default Web Site website' do
       describe iis_website('Default Web Site') do
         it { should exist }
         it { should be_enabled }
@@ -32,7 +33,7 @@ describe 'A Web Server' do
       end
 
       describe 'with virtual directory configured for CDP' do
-        describe iis_website('Default Website') do
+        describe iis_website('Default Web Site') do
           it{ should have_virtual_dir('cdp').with_path('C:\\inetpub\\cdp') }
         end
 
@@ -45,9 +46,11 @@ describe 'A Web Server' do
           end
         end
       end
+    end
 
+    describe 'has a /cps virtual directory in the Default Web Site website' do
       describe 'with virtual directory configured for CPS' do
-        describe iis_website('Default Website') do
+        describe iis_website('Default Web Site') do
           it{ should have_virtual_dir('cps').with_path('C:\\inetpub\\cps') }
         end
 
@@ -62,12 +65,18 @@ describe 'A Web Server' do
     end
   end
 
-  describe 'acting as a CEP endpoint' do
+  describe 'acting as a Certificate Enrollment Policy Web Server' do
   end
 
-  describe 'acting as a CEWS endpoint' do
+  describe 'acting as a Certificate Enrollment Web Server' do
   end
 
-  describe 'acting as an OCSP endpoint' do
+  describe 'acting as a Certification Authority Web Enrollment Server' do
+  end
+
+  describe 'acting as a Network Device Enrollment Server' do
+  end
+
+  describe 'acting as an Online Responder' do
   end
 end
