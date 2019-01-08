d1b50b37739d2600a7691091d5949dc4ea9044a2
Stephen Hoekstra <shoekstra@schubergphilis.com>
2019-01-08 14:43:09 +0100

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -7,12 +7,12 @@ issues_url       'https://github.schubergphilis.com/shoekstra/chef-certificate_s
 source_url       'https://github.schubergphilis.com/shoekstra/chef-certificate_services'
 version          '0.1.0'
 
-recipe           'certificate_services::crl_distribution_point', 'Installs and configures IIS with virtual directories for CDP and CPS'
-recipe           'certificate_services::enterprise_subordinate_ca', 'Installs and configures an online Enterprise Subordinate CA'
-recipe           'certificate_services::standalone_root_ca', 'Installs and configures an offline Standalone Root CA'
+recipe 'certificate_services::crl_distribution_point', 'Installs and configures IIS with virtual directories for CDP and CPS'
+recipe 'certificate_services::enterprise_subordinate_ca', 'Installs and configures an online Enterprise Subordinate CA'
+recipe 'certificate_services::standalone_root_ca', 'Installs and configures an offline Standalone Root CA'
 
-supports         'windows', '= 6.3'
+supports 'windows', '= 6.3'
 
-depends          'iis', '~> 4.1.10'
-depends          'pspki', '= 0.1.0'
-depends          'windows', '= 1.44.1'
+depends 'iis'
+depends 'pspki', '~> 0.1'
+depends 'windows'
